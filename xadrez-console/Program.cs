12da64ea2c8e8455624a830e880a361f26ecd41d
andsGomes <anderson.stos.gomes@gmail.com>
2023-02-24 00:54:07 -0300

    Improving the implementation of the PrintBoard method

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,20 +7,22 @@ Console.Clear();
 
 try
 {
-  // Board board = new Board(8, 8);
+  Board board = new Board(8, 8);
 
-  // board.placePart(new Tower(Color.Black, board), new Position(0, 0));
-  // board.placePart(new Tower(Color.Black, board), new Position(1, 9));
+  board.placePart(new Tower(Color.Black, board), new Position(7, 0));
+  board.placePart(new Tower(Color.Black, board), new Position(7, 1));
 
-  // board.placePart(new King(Color.Black, board), new Position(2, 4));
-  // board.placePart(new King(Color.Black, board), new Position(0, 0));
+  board.placePart(new Tower(Color.White, board), new Position(3, 6));
+  board.placePart(new Tower(Color.White, board), new Position(2, 1));
 
-  // ScreenBoard.PrintBoard(board);
+  board.placePart(new King(Color.Blue, board), new Position(1, 2));
+  board.placePart(new King(Color.Black, board), new Position(1, 5));
 
+  ScreenBoard.PrintBoard(board);
 
-  ChessPosition pos = new ChessPosition('A', 2);
-  System.Console.WriteLine(pos); 
-  System.Console.WriteLine(pos.toPosition());
+  // ChessPosition pos = new ChessPosition('A', 2);
+  // System.Console.WriteLine(pos); 
+  // System.Console.WriteLine(pos.toPosition());
 
 }
 catch (BoardException e)
